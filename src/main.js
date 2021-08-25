9bedeae43feb9e7356523c011bbfa24dbe5e571a
elmtree-Ahn <clfclf3138@gmail.com>
2021-08-26 06:51:56 +0900

    프로젝트 완료. 그런데, JS가 상당히 어렵다. 무한 복습필요!!

---
+++ src/main.js
@@ -0,0 +1,51 @@
+
+// Fetch the items from the JSON file
+function loadItems() {
+  return fetch('data/data.json')
+  .then(response => response.json())
+  .then(json => json.items);  
+}
+
+// 아이템 업데이트
+function displayItems(items) {
+  const container = document.querySelector('.items');
+  container.innerHTML = items.map(item => createHTMLString(item)).join('');
+}
+
+// html 리스트 만들기
+function createHTMLString(item) {
+  return `
+    <li class="item">
+      <img src="${item.image}" alt="${item.type}" class="item__thumnail">
+      <span class="item__description">${item.gender}, ${item.size}</span>
+    </li>
+    `;
+}
+
+function onButtonClick (event, items) {
+  const dataset = event.target.dataset;
+  const key = dataset.key;
+  const value = dataset.value;
+
+  if (key == null || value == null) {
+    return;
+  }
+
+  displayItems(items.filter(item => item[key] == value))
+}
+
+function setEventListeners(items) {
+  const logo = document.querySelector(".logo")
+  const buttons = document.querySelector('.buttons')
+  logo.addEventListener('click', () => displayItems(items))
+  buttons.addEventListener('click', event => onButtonClick(event, items))
+}
+
+//main
+loadItems()
+  .then(items => {
+    console.log(items)
+    displayItems(items);
+    setEventListeners(items)
+  })
+  .catch(console.log);
